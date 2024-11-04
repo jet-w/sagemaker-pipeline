ff83ffb754a49924e79c52f01c7dae815a2b34d9
local_wangh <local_wangh@EDC294556.UniNet.unisa.edu.au>
2024-11-05 09:45:14 +1030

    auto upload message

--- etc/training.py
+++ etc/training.py
@@ -6,4 +6,7 @@ sklearn_framework_version = "1.2-1"
 hyperparameters = {"epochs": training_epochs}
 tensorflow_version = "2.4.1"
 python_version = "py37"
-    
+
+
+# Where to store the trained model
+model_path = f"s3://{bucket}/humansystem/models/predict/"
