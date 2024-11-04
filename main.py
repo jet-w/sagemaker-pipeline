d546bede82d058d5adf7dcfe2bdec3dc88027472
local_wangh <local_wangh@EDC294556.UniNet.unisa.edu.au>
2024-11-04 15:35:26 +1030

    auto upload message

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(__file__))
 from hs_pipeline import get_pipeline
-from etc.conf import role_arn
+from etc import role_arn
 
 if __name__ == "__main__":
     pipeline = get_pipeline()
