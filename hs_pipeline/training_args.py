eb67f468f06bbc622b12b9d613c6a80783a33c65
unknown <unisa.dady@gmail.com>
2024-11-04 22:47:15 +1030

    modify common msg

--- hs_pipeline/training_args.py
+++ hs_pipeline/training_args.py
@@ -18,7 +18,7 @@ def get_sklean_training_args(pipeline_session, step_process):
         py_version       = python_version,
         sagemaker_session= pipeline_session,
     )
-    
+    print(step_process.properties.ProcessingOutputConfig.Outputs["test"].S3Output.S3Uri)
     model_s3 = os.path.dirname(step_process.properties.ProcessingOutputConfig.Outputs["test"].S3Output.S3Uri)
     model_s3 = f"{model_s3}/model"
 
