ff79b0b49e5cf8dc758f108073fd59cd07729a61
local_wangh <local_wangh@EDC294556.UniNet.unisa.edu.au>
2024-11-07 00:32:13 +1030

    auto upload message

--- src/steps/evaluation/evaluation_svm.py
+++ src/steps/evaluation/evaluation_svm.py
@@ -47,7 +47,7 @@ def get_model(model_dir):
     
     model_files = []
     for p, _, files in os.walk(model_dir):
-        svm_modles = filter(lambda x: x == "svm_model.joblib", files)
+        svm_modles = filter(lambda x: x.lower().endswith(".joblib"), files)
         model_files.extend([os.path.join(p, svm) for svm in svm_modles])
     
     model_path = model_files[0]
