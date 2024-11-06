2bd135087dbd7fbd976eb36a81e36e4cea2f4d47
local_wangh <local_wangh@EDC294556.UniNet.unisa.edu.au>
2024-11-06 16:56:55 +1030

    auto upload message

--- src/steps/preprocess/preprocess.py
+++ src/steps/preprocess/preprocess.py
@@ -9,22 +9,24 @@ if __name__ == "__main__":
     df = None
     logging.info("Start preprocess")
     df_list = []
-    for p, _, files in os.walk(f"{base_dir}/input"):
-        logging.info(p)
-        logging.info(files)
-        
-        df_list.extend(list(
-                    map(
-                        lambda x: pd.read_csv(os.path.join(p, x)), 
-                        filter(lambda f: f.lower().endswith(".csv"), files)
-                    )
-                ))
-    if len(df_list) == 0:
-        raise "Empty file"
-    df = pd.concat( df_list) 
-    rows = df.shape[0]
-    logging.info(f"Start preprocess: {rows} records")
+    #for p, _, files in os.walk(f"{base_dir}/input"):
+    #    logging.info(p)
+    #    logging.info(files)
+    #    
+    #    df_list.extend(list(
+    #                map(
+    #                    lambda x: pd.read_csv(os.path.join(p, x)), 
+    #                    filter(lambda f: f.lower().endswith(".csv"), files)
+    #                )
+    #            ))
+    #if len(df_list) == 0:
+    #    raise "Empty file"
+    #df = pd.concat( df_list) 
+    #rows = df.shape[0]
+    #logging.info(f"Start preprocess: {rows} records")
     # Sample data
+
+    df = pd.read_csv(os.path.join(f"{base_dir}/input", "Indicators_Intervention-20241106.csv"))
     training = df.sample(frac=0.8, random_state=42)
     test = df.drop(training.index)
 
