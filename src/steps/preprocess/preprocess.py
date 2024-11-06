8b902bc66e4fc48f7972e4b986aa1c704aa8d9e4
local_wangh <local_wangh@EDC294556.UniNet.unisa.edu.au>
2024-11-06 16:43:23 +1030

    auto upload message

--- src/steps/preprocess/preprocess.py
+++ src/steps/preprocess/preprocess.py
@@ -1,16 +1,26 @@
 import os
 import pandas as pd
-
+import logging
 #####################################################
 ## Will be replaced using code in the future
 base_dir        = "/opt/ml/processing"
 
 if __name__ == "__main__":
     df = None
+    logging.info("Start preprocess")
     for p, _, files in os.walk(f"{base_dir}/input"):
-        df = pd.concat(list(map(lambda x: pd.read_csv(os.path.join(p, x)), files)))
+        logging.info(p)
+        logging.info(files)
+        df = pd.concat(
+                list(
+                    map(
+                        lambda x: pd.read_csv(os.path.join(p, x)), 
+                        filter(lambda f: f.lower().endswith(".csv"), files)
+                    )
+                )
+             )
     rows = df.shape[0]
-    
+    logging.info(f"Start preprocess: {rows} records")
     # Sample data
     training = df.sample(frac=0.8, random_state=42)
     test = df.drop(training.index)
