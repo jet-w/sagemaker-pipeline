c5580bd25c5ece2121fa4d8abf0888cfa3781318
local_wangh <local_wangh@EDC294556.UniNet.unisa.edu.au>
2024-11-07 11:54:09 +1030

    auto upload message

--- src/hs_pipeline/hs_pipeline.py
+++ src/hs_pipeline/hs_pipeline.py
@@ -96,5 +96,5 @@ def get_pipeline():
         #steps=[step_process, step_train_model, step_evaluate_model],
         #steps=[step_process, step_train_model],
         #steps=[step_evaluate_model, step_cond]
-        steps = [step_cond]
+        steps = [register_step]
     )
