1ebbdd4191f02edc56d86e04c514f84d682b66a8
unknown <unisa.dady@gmail.com>
2024-11-10 21:35:23 +1030

    modify common msg

--- src/hs_pipeline/hs_pipeline.py
+++ src/hs_pipeline/hs_pipeline.py
@@ -180,7 +180,7 @@ def get_pipeline():
             accuracy_mse_threshold,
         ],
         #steps=[step_process, step_train_model, step_evaluate_model, step_cond],
-        steps=[step_process, step_train_model]#, step_evaluate_model, step_register],
+        steps=[step_process, step_train_model, step_evaluate_model]#, step_register],
         #steps=[step_process, step_train_model, step_evaluate_model, step_cond]
         #steps=[step_process, step_train_model],
         #steps=[step_evaluate_model, step_cond]
