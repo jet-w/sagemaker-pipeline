10df6232cff24f2dae977ceef63206f1926197fb
local_wangh <local_wangh@EDC294556.UniNet.unisa.edu.au>
2024-11-11 14:46:35 +1030

    auto upload message

---
+++ src/steps/deployment/deployment_args-02.py
@@ -0,0 +1,52 @@
+from sagemaker.processing import ScriptProcessor
+from sagemaker.workflow.parameters import ParameterString
+from sagemaker.workflow.steps import ProcessingStep
+
+from etc import *
+
+# Define parameters for deployment
+#model_s3_uri = "s3://path-to-your-model-artifact/model.tar.gz"
+#endpoint_name = "my-endpoint-name"
+instance_type = "ml.m5.large"
+#role_arn = "<your-sagemaker-role-arn>"
+endpoint_name = "HS-endpoint-Intervention-Recommendation"
+
+def get_step_deployment(session, step_register):
+    # Initialize the ScriptProcessor
+    script_processor = ScriptProcessor(
+        
+        command=["python3"],
+        instance_type=deployment_exec_instance_type,
+        instance_count=1,
+        role=role,
+        sagemaker_session=session
+    )
+    
+    # Run the ScriptProcessor to deploy the model
+    script_processor.run(
+        code="./steps/deployment/deploy.py",
+        arguments=[
+            #"--model-s3-uri", model_s3_uri,
+            #"--model-package-arn", step_register.properties.ModelPackageArn,
+            "--endpoint-name", endpoint_name,
+            "--instance-type", deployment_instance_type,
+            "--role-arn", role_arn
+        ]
+    )
+    
+    
+    #deployment_step = ProcessingStep(
+    return ProcessingStep(
+        name="DeployModelStep",
+        processor=script_processor,
+        #step_args=script_processor.run(
+        #    code="./steps/deployment/deploy.py",
+        #    arguments=[
+        #        #"--model-s3-uri", model_s3_uri,
+        #        #"--model-package-arn", step_register.properties.ModelPackageArn,
+        #        "--endpoint-name", endpoint_name,
+        #        "--instance-type", deployment_instance_type,
+        #        "--role-arn", role_arn
+        #    ]
+        #)
+    )
